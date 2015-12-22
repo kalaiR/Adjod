37ef670bc0a21e76108c63e60fe75cbef18dce62
kalaiR <kalaimca.gs@gmail.com>
2015-12-22 15:27:15 +0530

    update the directories in settings file

--- adjod/adjod/settings.py
+++ adjod/adjod/settings.py
@@ -50,7 +50,7 @@ USE_L10N = True
 USE_TZ = True
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__), '../media')
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
@@ -74,7 +74,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(__file__), '../static'),
+    os.path.join(os.path.dirname(__file__), 'static'),
 )
 
 # List of finder classes that know how to find static files in various locations.
@@ -132,7 +132,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(__file__), '../templates'),
+    os.path.join(os.path.dirname(__file__), 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -259,7 +259,7 @@ SEO_MODEL_REGISTRY = (
 )
 
 # settings for GEO
-GEOIP_PATH = os.path.join(os.path.dirname(__file__), '../static/geoip/')
+GEOIP_PATH = os.path.join(os.path.dirname(__file__), 'static/geoip/')
 
 # settings for Currency Conversion
 DJANGO_MONEY_RATES = {
